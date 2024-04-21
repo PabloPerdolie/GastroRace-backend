380ae66a1b3eada68c475e56bee12a8a32b72723
Pablo <parusov.pashka@ya.ru>
2024-04-21 14:24:14 +0300

    cors policy

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -3,11 +3,18 @@ package handlers
 import (
 	"backend/internal/handlers/product"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"net/http"
 )
 
 func SetupRoutes() {
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000", "https://gastrorace-frontend.onrender.com"},
+	})
+
 	r := mux.NewRouter()
+	r.Use(c.Handler)
+
 	http.Handle("/", r)
 
 	r.HandleFunc("/product", product.Create).Methods("POST")
